0dc40fb98a00308b27ea8ade1bfe8494221df3f0
JeremyTsui <44908010+rdhuht@users.noreply.github.com>
2020-12-29 00:46:33 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,29 +1,37 @@
-let tm = TM1637.create(
+input.onGesture(Gesture.Shake, function () {
+    ajustTM()
+})
+function ajustTM () {
+    tm.showDP(1, false)
+    while (!(input.isGesture(Gesture.ScreenUp))) {
+        timeanddate.numericTime(function (hour, minute, second, month, day, year) {
+            if (input.buttonIsPressed(Button.A)) {
+                if (hour >= 24) {
+                    timeanddate.set24HourTime(0, minute, 0)
+                }
+                timeanddate.set24HourTime(hour + 1, minute, 0)
+                basic.pause(200)
+            }
+            if (input.buttonIsPressed(Button.B)) {
+                if (minute >= 60) {
+                    timeanddate.set24HourTime(hour, 0, 0)
+                }
+                timeanddate.set24HourTime(hour, minute + 1, 0)
+                basic.pause(200)
+            }
+            tm.showNumber(hour * 100 + minute)
+        })
+    }
+}
+let tm: TM1637.TM1637LEDs = null
+tm = TM1637.create(
 DigitalPin.P1,
 DigitalPin.P2,
 7,
 4
 )
 timeanddate.set24HourTime(12, 0, 0)
-while (!(input.isGesture(Gesture.ScreenUp))) {
-    timeanddate.numericTime(function (hour, minute, second, month, day, year) {
-        if (input.buttonIsPressed(Button.A)) {
-            if (hour >= 24) {
-                timeanddate.set24HourTime(0, minute, 0)
-            }
-            timeanddate.set24HourTime(hour + 1, minute, 0)
-            basic.pause(200)
-        }
-        if (input.buttonIsPressed(Button.B)) {
-            if (minute >= 60) {
-                timeanddate.set24HourTime(hour, 0, 0)
-            }
-            timeanddate.set24HourTime(hour, minute + 1, 0)
-            basic.pause(200)
-        }
-        tm.showNumber(hour * 100 + minute)
-    })
-}
+ajustTM()
 basic.forever(function () {
     tm.showDP(1, true)
     timeanddate.numericTime(function (hour, minute, second, month, day, year) {
